86291c5675e3ea74424b7ca5abebf587b1d6c247
Jonathan Ehman <jpehman@gmail.com>
2025-04-25 20:54:06 -0500

    Update node.js.yml

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,15 @@
 import typescript from "@rollup/plugin-typescript";
-import commonjs from '@rollup/plugin-commonjs';
-import terser from '@rollup/plugin-terser';
-import pkg from './package.json' with { type: "json" };
+import commonjs from "@rollup/plugin-commonjs";
+import terser from "@rollup/plugin-terser";
+import pkg from "./package.json" with { type: "json" };
 
 export default [
   {
-    input: 'src/pause-me.ts',
+    input: "src/pause-me.ts",
     output: [
-      { file: pkg.main, format: 'umd', exports: "default", name: "pauseMe" },
-      { file: pkg.common, format: 'cjs', exports: "default", name: "pauseMe" },
-      { file: pkg.module, format: 'es', exports: "default", name: "pauseMe" }
+      { file: pkg.main, format: "umd", exports: "default", name: "pauseMe" },
+      { file: pkg.common, format: "cjs", exports: "default", name: "pauseMe" },
+      { file: pkg.module, format: "es", exports: "default", name: "pauseMe" }
     ],
     plugins: [
       typescript(),
